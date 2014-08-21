a775c87fdf8c46aebacd3ac40946e1618e19ffa7
andresjordanze <andresjordanze@gmail.com>
2014-08-21 10:25:14 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agucb::Application.config.secret_key_base = 'b27ecb5d88c525125f25d8053ed119c3ab8236598e6ae1384bc7d8ee211de3b8cdb574849135d886f81cf5e4f274360b15d24e518b1b3acfbb8a0b82b490faeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Agucb::Application.config.session_store :cookie_store, key: '_agucb_session'
