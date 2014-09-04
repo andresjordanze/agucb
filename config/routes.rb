1b328cf0e55060713606089499a97d763a158619
andresjordanze <andresjordanze@gmail.com>
2014-09-04 11:41:34 -0400

    css dividido

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Agucb::Application.routes.draw do
   root "home#index"
+
+  match '/index_futbol',    to: 'home#index_futbol',    via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
