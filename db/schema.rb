a1da21ffc0dc816596835f02450cdf1d621530a0
Leonardo <leo_mejia_p@hotmail.com>
2014-11-06 11:47:36 -0400

    modificando vista login XD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106142248) do
+ActiveRecord::Schema.define(version: 20141106145122) do
 
   create_table "associateds", force: true do |t|
     t.string   "name"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20141106142248) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "userType"
   end
 
 end
