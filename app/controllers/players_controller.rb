9be9d2e2365626c85826c69afd9aef6f224a643a
andresjordanze <andresjordanze@gmail.com>
2014-11-13 09:29:05 -0400

    crud jugadores

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -25,7 +25,6 @@ class PlayersController < ApplicationController
   # POST /players.json
   def create
     @player = Player.new(player_params)
-
     respond_to do |format|
       if @player.save
         format.html { redirect_to @player, notice: 'Player was successfully created.' }
