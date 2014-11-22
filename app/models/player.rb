65085c424d82f24d70dbec51871ba021e2fa6ce6
andresjordanze <andresjordanze@gmail.com>
2014-11-22 12:06:02 -0400

    fotos en jugador sin funcionar

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,12 @@
 class Player < ActiveRecord::Base
-	attr_accessible :name,:lastname,:career, :goals, :t_ama, :t_roj, :team_id
+	attr_accessible :name,:lastname,:career, :goals, :t_ama, :t_roj, :team_id, :photo
 	belongs_to :team
+
+	has_attached_file :photo, :styles => {  :medium => "300x300>", :thumb => "100x100>" },
+                  :url  => "/:basename.:extension",
+                  :path => ":Rails_root/:basename.:extension"
+
+	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
 	def full_name
 		self.name+" "+self.lastname
