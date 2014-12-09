3b13d3e706b02dd90ef4747b49ee6942cda884f6
Leonardo <leo_mejia_p@hotmail.com>
2014-12-09 01:26:22 -0400

    agregando foto a actividad

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -69,6 +69,6 @@ class ActivitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def activity_params
-      params.require(:activity).permit(:title, :description, :location)
+      params.require(:activity).permit(:title, :description, :location, :photo)
     end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ActiveRecord::Base
-	attr_accessible :title,:description, :location
+	attr_accessible :title,:description, :location, :photo
+
+	has_attached_file :photo
+  	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
